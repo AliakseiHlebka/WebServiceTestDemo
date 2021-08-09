75468bffce4c2d0fcf811865240d4be02bc1a564
Aliaksei Hlebka <aliaksei_hlebka@epam.com>
2021-08-09 22:50:14 +0300

    Refactor naming for classes and methods

--- src/test/java/com/epam/webservices/demo/tests/JsonplaceholderTypicodeUsersTest.java
+++ src/test/java/com/epam/webservices/demo/tests/JsonplaceholderTypicodeUsersTest.java
@@ -10,31 +10,31 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.response.ResponseBody;
 
-public class RestAssuredTests {
+public class JsonplaceholderTypicodeUsersTest {
+
+    Response response;
 
     @BeforeTest
     public void initTest() {
         RestAssured.baseURI = "https://jsonplaceholder.typicode.com/users";
+        response = RestAssured.when().get().andReturn();
     }
 
     @Test
     public void verifyStatusCode() {
-        Response response = RestAssured.when().get().andReturn();
         Assert.assertTrue(response.getStatusLine().contains("200 OK"),
             "Status code differs from the expected one");
     }
 
     @Test
-    public void verifyResponseHeader() {
-        Response response = RestAssured.when().get().andReturn();
+    public void verifyContentTypeHeader() {
         String responseContentTypeHeader = response.getHeader("Content-Type");
         Assert.assertEquals(responseContentTypeHeader, "application/json; charset=utf-8",
             "Value of Content-Type header differs from the expected one");
     }
 
     @Test
-    public void verifyResponseBody() {
-        Response response = RestAssured.when().get().andReturn();
+    public void verifyResponseBodyArrayLength() {
         ResponseBody responseBody = response.getBody();
         User[] users = responseBody.as(User[].class);
         Assert.assertEquals(users.length, 10,
